d63d019ea86fca0a9fbd5ec373f7fc4fa992fe60
Cayetano H. Osma <moesis@gmail.com>
2019-08-15 22:32:45 +0100

    Modify Docker-compose to export the Node debug port include in package.json the debugging option to Nodemon TagController now it is working with promises, but dont return the res
    Working with Connections and Mongoose

--- api/src/app/controllers/TagsController.js
+++ api/src/app/controllers/TagsController.js
@@ -38,71 +38,104 @@ export default class TagsController {
    * Returns the data with books list.
    *
    * @returns {{data: {books: *[]}}}
+   * @deprecated
    */
+  OldgetAll = (req, res) => {
+
+    let connection = this.client.get()
+
+    // eslint-disable-next-line no-console
+    console.log('connection:', connection)
+
+    TagModel.find({}, null, (err, tags) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log('ERROR:', err)
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('tags => ', tags)
+
+        res.status = HTTP_CODES.HTTP_OK
+        res.locals.data = {
+          'data': {
+            'tags': [
+              tags,
+            ],
+          },
+        }
+      }
+    })
+
+    return res
+  }
+
   getAll = (req, res) => {
 
+    let resources;
+
     this.client.get()
-    TagModel.find({}, null)
-    .then ((tags, res) => {
-      res.status = HTTP_CODES.HTTP_OK
+    TagModel.find({}, null).exec()
+    .then(function (tags) {
+      resources = res;
       // eslint-disable-next-line no-console
       console.log('tags => ', tags)
 
-      return tags;
+      resources.status = HTTP_CODES.HTTP_OK
+      resources.locals.data = {
+        'data': {
+          'tags': [
+            {
+              'code': tags.code,
+              'text': tags.text
+            }
+          ],
+        },
+      }
+
+      return resources;
     })
-    .catch( (err, res) => {
-      res.status = HTTP_CODES.HTTP_INTERNAL_SERVER_ERROR
+    .catch(function(err){
       // eslint-disable-next-line no-console
       console.log('ERROR:', err)
     });
-
-    //
-    // res.status = status;
-    // res.locals.data = {
-    //   'data': {
-    //     'tags': xxx,
-    //   },
-    // }
-
-    return res;
   }
 
-save = (req, res) => {
-  res.status = HTTP_CODES.HTTP_OK
+  save = (req, res) => {
+    res.status = HTTP_CODES.HTTP_OK
 
-  if (req.is('json') !== 'json') {
-    res.status = HTTP_CODES.HTTP_BAD_REQUEST
-    res.locals.data = {
-      'data': {
-        'message': 'The body must be in JSON format.',
-      },
+    if (req.is('json') !== 'json') {
+      res.status = HTTP_CODES.HTTP_BAD_REQUEST
+      res.locals.data = {
+        'data': {
+          'message': 'The body must be in JSON format.',
+        },
+      }
+
+      return res
+    }
+
+    let tag = new TagModel()
+    tag.code = req.body.code
+    tag.tagText = req.body.text
+
+    try {
+      const result = tag.save()
+      res.locals.data = {
+        'data': {
+          'message': 'Data successfully saved',
+          'extra': result,
+        },
+      }
+    } catch (err) {
+      res.status = HTTP_CODES.HTTP_INTERNAL_SERVER_ERROR
+      res.locals.data = {
+        'data': {
+          'message': err,
+        },
+      }
+      return res
     }
 
     return res
   }
-
-  let tag = new TagModel()
-  tag.code = req.body.code
-  tag.tagText = req.body.text
-
-  try {
-    const result = tag.save()
-    res.locals.data = {
-      'data': {
-        'message': 'Data successfully saved',
-        'extra': result,
-      },
-    }
-  } catch (err) {
-    res.status = HTTP_CODES.HTTP_INTERNAL_SERVER_ERROR
-    res.locals.data = {
-      'data': {
-        'message': err,
-      },
-    }
-    return res
-  }
-
-  return res
-}
 }
